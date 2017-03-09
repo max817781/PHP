0c1ca9d2ed20aff29831a0624edcebef7e7641aa
TsaoYuCheng <max817781@gmail.com>
2017-03-09 14:34:21 +0800

    test

---
+++ 1.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo $_GET['id'];
+        echo '<br>';
+        echo $_GET['pwd'];
+               
+
+
+        ?>
+    </body>
+</html>
---
+++ GET.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <form action="1.php" method="get">
+           帳號:<input type="text" name="id">
+           密碼:<input type="password" name="pwd">
+           <input type="submit" value="登入"><input type="reset" value="清除">
+        </form>
+    </body>
+</html>
